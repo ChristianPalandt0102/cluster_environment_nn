4f9ba3de998f46e0d4c6ac996a2de215238b7a16
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 17:26:37 +0200

    Aktualisieren von dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,19 @@
+
+def message(evt):
+    data = json.loads(evt.data)
+
+    log(f"[SIGNAL] {data}")
+
+    # send signal to JS world
+    from browser import window
+    window.dispatchEvent(
+        window.CustomEvent.new(
+            "sandbox_signal",
+            {"detail": data}
+        )
+    )
+
+
 # inside dashboard.py
 
 from factory_bus import FACTORY

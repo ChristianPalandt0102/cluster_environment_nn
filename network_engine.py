5e215a1726389d670c708821c182e2ceca5b41be
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 17:03:38 +0200

    Aktualisieren von network_engine.py

--- network_engine.py
+++ network_engine.py
@@ -1,3 +1,27 @@
+
+
+class AdaptiveRing:
+
+    def __init__(self, nodes):
+        self.nodes = nodes
+        self.activity = {n:0 for n in nodes}
+
+    def signal(self, node):
+        self.activity[node] += 1
+
+    def rewire(self):
+
+        # sort by activity
+        ordered = sorted(
+            self.activity,
+            key=lambda n:self.activity[n],
+            reverse=True
+        )
+
+        self.nodes = ordered
+        print("[ROUTING] ring evolved:", self.nodes)
+
+
 gpu_mind = ConsciousnessGPU()
 
 def handle_packet(packet):

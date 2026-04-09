aee0aa2ce5e12458a1e81f4ae249ad06d9f0a426
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 17:43:38 +0200

    Aktualisieren von main_cortex.py

--- main_cortex.py
+++ main_cortex.py
@@ -1,2 +1,15 @@
+
+from hidden_layer_loop import HiddenLayerLoop
+
+hidden_layer = HiddenLayerLoop(
+    dream_engine,
+    class_builder,
+    temporal_memory,
+    consensus,
+    energy
+)
+
+asyncio.create_task(hidden_layer.cycle())
+
 if msg["type"] == "template_mutation":
     template_db.integrate(msg["template"])

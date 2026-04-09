ab3c7f33ecd09646183e7e83f1612665471ebf30
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 17:30:05 +0200

    Aktualisieren von dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,33 @@
 
+from browser import document, websocket
+import json
+
+term = document["terminal"]
+
+def log(msg):
+    term <= f"{msg}<br>"
+    term.scrollTop = term.scrollHeight
+
+
+# --- WEBSOCKET OBSERVER ---
+ws = websocket.WebSocket("ws://localhost:8765")
+
+@ws.bind("open")
+def open(evt):
+    log("[CONNECTED] observer stream")
+
+@ws.bind("message")
+def message(evt):
+    data = json.loads(evt.data)
+
+    log(f"[SIGNAL] {data}")
+
+@ws.bind("close")
+def close(evt):
+    log("[DISCONNECTED]")
+
+
+
 def message(evt):
     data = json.loads(evt.data)
 

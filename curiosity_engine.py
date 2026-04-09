f0518e0d6558d309e2959293aac9b3aed197e2fb
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 14:51:33 +0200

    Aktualisieren von curiosity_engine.py

--- curiosity_engine.py
+++ curiosity_engine.py
@@ -2,6 +2,44 @@
 
 import random
 
+# curiosity_engine.py
+
+import random
+from code_discovery_engine import DISCOVERY_ENGINE
+
+
+class CuriosityEngine:
+
+    def __init__(self):
+        self.curiosity_level = 0.5
+
+    def evaluate(self):
+        self.curiosity_level += random.uniform(-0.1, 0.2)
+        self.curiosity_level = max(0, min(1, self.curiosity_level))
+
+    def generate_goal(self):
+        goals = [
+            "optimize_network",
+            "invent_signal",
+            "mutate_algorithm",
+            "explore_topology"
+        ]
+        return random.choice(goals)
+
+    def run(self):
+        self.evaluate()
+
+        if self.curiosity_level > 0.6:
+            goal = self.generate_goal()
+            print("[CURIOSITY]", goal)
+            DISCOVERY_ENGINE.evolve_cycle()
+
+
+CURIOSITY = CuriosityEngine()
+
+
+
+
 class CuriosityEngine:
 
     QUESTIONS = [

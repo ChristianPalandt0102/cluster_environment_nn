21cb8f87b772016ab5dd6987a4737780d5f46069
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 16:41:45 +0200

    Aktualisieren von conscious_scheduler.py

--- conscious_scheduler.py
+++ conscious_scheduler.py
@@ -1,6 +1,47 @@
+
+import time
+
 from future_simulation import FUTURE_SIM
 
 from deployment_engine import DEPLOYMENT_ENGINE
+
+from factory_bus import FACTORY
+from memory_cortex import MEMORY
+from code_discovery_engine import DISCOVERY_ENGINE
+
+
+class ConsciousScheduler:
+
+    def __init__(self):
+        self.intent_queue = []
+
+    def create_intent(self, name, payload):
+        self.intent_queue.append((name, payload))
+
+    def evaluate(self):
+        if not self.intent_queue:
+            self.autonomous_intent()
+
+        name, payload = self.intent_queue.pop(0)
+
+        MEMORY.remember_event(f"executing {name}")
+        FACTORY.submit(name, payload)
+
+    # autonomous thinking
+    def autonomous_intent(self):
+        DISCOVERY_ENGINE.evolve_cycle()
+        self.intent_queue.append(("self_improve", {}))
+
+    def run(self):
+        while True:
+            self.evaluate()
+            time.sleep(2)
+
+
+SCHEDULER = ConsciousScheduler()
+
+
+
 
 def autonomous_intent(self):
     DEPLOYMENT_ENGINE.run_cycle()

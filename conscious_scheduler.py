ed9d986afb0882f3515ed2e1b21d2ca2a31741c1
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 14:56:44 +0200

    Aktualisieren von conscious_scheduler.py

--- conscious_scheduler.py
+++ conscious_scheduler.py
@@ -1,4 +1,11 @@
 from future_simulation import FUTURE_SIM
 
+from deployment_engine import DEPLOYMENT_ENGINE
+
+def autonomous_intent(self):
+    DEPLOYMENT_ENGINE.run_cycle()
+    self.intent_queue.append(("self_expand", {}))
+
+
 best = FUTURE_SIM.choose(name)
 FACTORY.submit(best["intent"], payload)

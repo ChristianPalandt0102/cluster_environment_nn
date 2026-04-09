87de9ed0bfa5810b01ec6cf6dd90ab1dbfe6cb45
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 18:12:26 +0200

    Aktualisieren von conscious_router.py

--- conscious_rourer.py
+++ conscious_rourer.py
@@ -1,3 +1,26 @@
+
+# conscious_router.py
+
+class ConsciousRouter:
+
+    def __init__(self, cortex):
+        self.cortex = cortex
+
+    def choose_route(self, packet, routes):
+
+        history = self.cortex.query(packet["type"])
+
+        scored = []
+
+        for r in routes:
+            score = len(history) + r["health"]
+            scored.append((score, r))
+
+        scored.sort(reverse=True)
+
+        return scored[0][1]
+
+
 def quantum_choose(self, signal, operation_boxes):
 
     snapshot = operation_boxes.collect_snapshot()

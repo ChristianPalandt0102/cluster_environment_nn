88e6b938a1d28a88e8682465c8d4d672a6b7deac
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 14:20:06 +0200

    nn_bus.py erstellen

---
+++ nn_bus.py
@@ -0,0 +1,44 @@
+# nn_bus.py
+
+import numpy as np
+import random
+
+
+class CNNLayer:
+    def __init__(self, kernel_size=3):
+        self.kernel = np.random.randn(kernel_size, kernel_size)
+
+    def forward(self, x):
+        h, w = x.shape
+        k = self.kernel.shape[0]
+        out = np.zeros((h-k+1, w-k+1))
+
+        for i in range(out.shape[0]):
+            for j in range(out.shape[1]):
+                region = x[i:i+k, j:j+k]
+                out[i, j] = np.sum(region * self.kernel)
+
+        return np.tanh(out)
+
+
+class NNBus:
+
+    def __init__(self):
+        self.layers = [
+            CNNLayer(3),
+            CNNLayer(3)
+        ]
+
+    def encode_signal(self, signal_matrix):
+        x = signal_matrix
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+    def route_score(self, encoded):
+        return float(np.mean(encoded))
+
+    def process(self, signal):
+        encoded = self.encode_signal(signal)
+        score = self.route_score(encoded)
+        return encoded, score

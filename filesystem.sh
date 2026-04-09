4c6514a7f2bdb7bb13dc6653790c6a798abf8bc8
ChristianPalandt0102 <Christian.Palandt@web.de>
2026-04-09 17:33:54 +0200

    filesystem.sh erstellen

---
+++ filesystem.sh
@@ -0,0 +1,19 @@
+mkdir -p core sandbox cognition observer cache database routing kernel
+
+touch core/{main_cortex.py,director.py,network_engine.py,neural_router.py,sandbox_scheduller.py}
+
+touch sandbox/{sandbox_grid_engine.py,sandbox_dna_compiler.py,classbuilder.py,function_builder.py}
+
+touch cognition/{dream_engine.py,hidden_layer_loop.py,observer_engine.py}
+
+touch observer/{observer_ws_stream.py,dashboard.py}
+
+touch cache/L5_cache.py
+
+touch database/{client.json,system.log.json,network.log.json,template_db.py}
+
+touch routing/{routing_table.py,ping_table.py}
+
+touch kernel/self_writing_kernel.py
+
+touch requirements.txt run.sh
